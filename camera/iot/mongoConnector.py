b32e53fbcefc620214b7b5cf5960cafd51fc845a
Digvijay Thakur <thakurds@cardiff.ac.uk>
2024-04-17 16:14:44 +0100

    import bug fix

--- camera/iot/mongoConnector.py
+++ camera/iot/mongoConnector.py
@@ -1,6 +1,7 @@
 import serial
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import time
 uri = ("mongodb+srv://digivj05:ypvFt1gqYqJnZW8f@cluster0.esdatxw.mongodb.net/?retryWrites=true&w=majority&appName"
        "=Cluster0")
 # Create a new client and connect to the server
